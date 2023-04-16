a57db1828d547e4fa6b47acfc9038c8b81b558cf
YU SHAO-HUNG (游紹宏) <hank199599@gmail.com>
2023-04-16 22:59:27 +0800

    feat: change desidn

--- public/annual_activity/eventList.js
+++ public/annual_activity/eventList.js
@@ -1,20 +1,32 @@
 var eventList = 
   [
       {
+        "title":"Solution Challenge",
+        "description":"Solution Challenge 是由 Google 面向全球大學生舉辦的年度比賽，旨在鼓勵學生利用 Google 的產品或平台，解決當地或全球社區面臨的問題，並為未來的職業發展提供實踐機會。比賽的主題涵蓋多個領域，例如教育、健康、環保、社會公益等，參賽者可根據自己的興趣和專業背景選擇相應的主題進行解決方案開發。比賽過程中，Google 提供了一系列的線上資源和技術支援，以幫助參賽者成功實現他們的解決方案。比賽分為初賽和決賽兩個階段，初賽中，參賽者需要提交一份概念書，經過評審後，獲選的隊伍將獲得 Google 的支援，繼續進行解決方案的開發。在決賽中，參賽者需要提交一份完整的解決方案和演示視頻，最終評審委員會將選出優勝隊伍。",
+        "pic_url":"../assets/img/event/solution challenge.gif",
+        "link":"https://developers.google.com/community/gdsc-solution-challenge?hl=zh-tw"
+      },
+      {
         "title":"International Women's Day",
         "description":"國際婦女節 (IWD) 是 Women Techmakers 最大的年度活動，大使們在 3 月和 4 月期間在世界各地舉辦活動以慶祝這一時刻。 從大型峰會到小型私密聚會， IWD 也是我們支持我們的使命的一種方式，即建設一個所有女性都能在科技領域茁壯成長的世界。 GDG 和 GDSC 社區也可以參與 IWD 活動。 ",
-        "pic_url":"../assets/img/IWD.png",
+        "pic_url":"../assets/img/event/IWD.png",
         "link":"https://developers.google.com/womentechmakers/initiatives/iwd"
       },
       {
         "title":"Google I/O Extended",
         "description":"Google I/O 將來自世界各地的開發人員聯繫在一起，進行深思熟慮的討論，與 Google 專家進行實踐學習，並搶先了解 Google 的最新開發人員產品。 I/O 的魔力並沒有在主賽事之後結束。 本地開發人員齊聚一堂參加 I/O Extended 活動，討論最新的新技術、總結內容、主持問答環節並會見其他技術愛好者。 ",
-        "pic_url":"../assets/img/io-extended-header.png",
+        "pic_url":"../assets/img/event/io-extended-header.png",
         "link":"https://gdg.community.dev/ioextended/"
       },
       {
-        "title":"DevFest",
-        "description":"DevFests 是由全球 Google Developer Group (GDG) 主持的當地技術會議。每個 DevFest 活動都是由當地發起人舉辦，以配合當地開發人員社群的需求和興趣。無論是透過實作學習體驗，還是由專家提供的當地語言技術講座，或是只要與其他當地開發人員開會，DevFest 的參與者就能學習如何透過 Google 的開發人員工具共同協作並創新。",
+        "title":"Summer Boot Camp",
+        "description":"Google Summer of Code 是一個全球性的在線計劃，專注於將新的貢獻者引入開源軟件開發。 GSoC 貢獻者在導師的指導下與開源組織合作進行為期 12 週以上的編程項目。",
+        "pic_url":"../assets/img/event/GSoC-Horizontal.png",
+        "link":"https://summerofcode.withgoogle.com/"
+      },
+      {
+        "title":"GDSC Leads Summit",
+        "description":"GDSC Leads Summit 是 Google Developer Student Clubs（GDSC）每年舉辦的研討會，旨在為全球 GDSC 社群的學生領袖們提供一個交流和學習的平台。該研討會主要針對 GDSC 的社群管理、技術培訓、活動策劃、專案管理等方面進行探討和分享。參加研討會的 GDSC Leads 不僅能夠與來自世界各地的學生領袖交流，還能參加由 Google 工程師和產品經理提供的技術培訓和知識分享。",
         "pic_url":"../assets/img/devfest.gif",
         "link":"https://developers.google.com/community/devfest?hl=zh-tw"
       }
